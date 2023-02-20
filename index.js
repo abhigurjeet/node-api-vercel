b71b4458a7c6d1c3f15d08c12c43dd0a133a4003
Gurjeet Singh <abhigurjeet@gmail.com>
2023-02-21 02:22:32 +0530

    Added html

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
-
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
 
 app.listen(3000, () => {
   console.log(`API listening on PORT 3000 `)
 })
 
 app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
+  res.render('index');
 })
 
 app.get('/about', (req, res) => {
