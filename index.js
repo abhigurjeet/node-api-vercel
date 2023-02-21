3ce4b1b84f476806c93b9408848be0f682448e43
Gurjeet Singh <abhigurjeet@gmail.com>
2023-02-21 18:13:33 +0530

    correcting views folder

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const ejs = require("ejs");
 const app = express();
 
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
@@ -14,7 +13,7 @@ app.listen(3000, () => {
 })
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index.ejs');
 })
 
 app.get('/about', (req, res) => {
